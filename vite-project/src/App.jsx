da48f31e93a78be40cd912a3c0c31efb0378abdf
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2026-04-25 00:02:59 +0300

    react background

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,25 +10,7 @@ function App() {
 
   return (
     <>
-
-<div style={{ width: '100%', height: '600px', position: 'relative' }}>
-  <LightRays
-    raysOrigin="top-center"
-    raysColor="#ffffff"
-    raysSpeed={1}
-    lightSpread={0.5}
-    rayLength={3}
-    followMouse={true}
-    mouseInfluence={0.1}
-    noiseAmount={0}
-    distortion={0}
-    className="custom-rays"
-    pulsating={false}
-    fadeDistance={1}
-    saturation={1}
-/>
-</div>
-
+  -pro
     <div style={{ width: '100%', height: '600px', position: 'relative' }}>
   <Beams
     beamWidth={3}
